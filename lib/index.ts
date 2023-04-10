2e892f84091ed929a6cad89c5fe8dcf3608ddd32
Daniel Lazar <lazardani0112@gmail.com>
2023-04-10 21:28:09 +0200

    fix: remove incomplete rule

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,5 @@
 import { decoratorArrayItemsRule } from './rules/decorator-array-items';
-import { decoratorPropertiesRule } from './rules/decorator-properties';
 
 export const rules = {
   'decorator-array-items': decoratorArrayItemsRule,
-  'decorator-properties': decoratorPropertiesRule,
 };
